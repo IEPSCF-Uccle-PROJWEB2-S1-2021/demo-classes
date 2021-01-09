2914322d6abddd2019c377516fc530b8d1737fd5
Francois Roland <froland@froland.be>
2021-01-09 16:42:54 +0100

    Write invoice classes
    Signed-off-by: Francois Roland <froland@froland.be>

--- invoice.js
+++ invoice.js
@@ -1,6 +1,39 @@
-class InvoiceLine {}
+class InvoiceLine {
+  constructor(product, unitPrice, quantity) {
+    this.product = product;
+    this.unitPrice = unitPrice;
+    this.quantity = quantity;
+  }
 
-class Invoice {}
+  total() {
+    return this.quantity * this.unitPrice;
+  }
+}
 
+class Invoice {
+  constructor() {
+    this.lines = [];
+  }
 
-module.exports = { Invoice, InvoiceLine }
+  add(line) {
+    this.lines.push(line);
+  }
+
+  subtotal() {
+    const subtotal = this.lines
+      .map((line) => line.total())
+      .reduce((accumulator, lineTotal) => accumulator + lineTotal);
+    return Math.round(subtotal * 100) / 100;
+  }
+
+  totalTax() {
+    const totalTax = this.subtotal() * .21;
+    return Math.round(totalTax * 100) / 100;
+  }
+
+  total() {
+    return this.subtotal() + this.totalTax();
+  }
+}
+
+module.exports = { Invoice, InvoiceLine };
--- invoice.test.js
+++ invoice.test.js
@@ -1,14 +1,53 @@
 const { Invoice, InvoiceLine } = require('./invoice');
 
 describe('Invoice', () => {
-
+  test('single line subtotal', () => {
+    const invoice = new Invoice();
+    const line = new InvoiceLine('crayon', 2, 10);
+    invoice.add(line);
+    expect(invoice.subtotal()).toEqual(20);
+  });
+  test('single line totalTax', () => {
+    const invoice = new Invoice();
+    const line = new InvoiceLine('crayon', 2, 10);
+    invoice.add(line);
+    expect(invoice.totalTax()).toEqual(4.2);
+  });
+  test('single line total', () => {
+    const invoice = new Invoice();
+    const line = new InvoiceLine('crayon', 2, 10);
+    invoice.add(line);
+    expect(invoice.total()).toEqual(24.2);
+  });
+  test('many lines subtotal', () => {
+    const invoice = new Invoice();
+    const line1 = new InvoiceLine('crayon', 2, 10);
+    const line2 = new InvoiceLine('stylo', 50, 3);
+    invoice.add(line1);
+    invoice.add(line2);
+    expect(invoice.subtotal()).toEqual(170);
+  });
+  test('many lines totalTax', () => {
+    const invoice = new Invoice();
+    const line1 = new InvoiceLine('crayon', 2, 10);
+    const line2 = new InvoiceLine('stylo', 50, 3);
+    invoice.add(line1);
+    invoice.add(line2);
+    expect(invoice.totalTax()).toEqual(35.70);
+  });
+  test('many lines total', () => {
+    const invoice = new Invoice();
+    const line1 = new InvoiceLine('crayon', 2, 10);
+    const line2 = new InvoiceLine('stylo', 50, 3);
+    invoice.add(line1);
+    invoice.add(line2);
+    expect(invoice.total()).toEqual(205.70);
+  });
 });
 
 describe('InvoiceLine', () => {
-  test.todo('single item montantHtva');
-  test.todo('single item montantTvac');
-  test.todo('single item montantTaxes');
-  test.todo('many items montantHtva');
-  test.todo('many items montantTvac');
-  test.todo('many items montantTaxes');
+  test('total', () => {
+    const line = new InvoiceLine('crayon', 2, 10);
+    expect(line.total()).toEqual(20);
+  });
 });
